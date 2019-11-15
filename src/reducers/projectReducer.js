b2f93cfe2e223449d90a3cb5fb74395f5319429c
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2019-11-15 16:00:44 -0500

    build reducer structure

---
+++ src/reducers/projectReducer.js
@@ -0,0 +1,10 @@
+export default function projectReducer(state={all_projects: []}, action) {
+  switch (action.type) {
+    case 'ADD_PROJECTS': {
+      debugger
+    }
+    default: {
+      return state
+    }
+  }
+}
