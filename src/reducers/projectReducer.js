5c53351f9cf0659c485f97a11f964f102ad8b7d3
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2019-11-16 21:51:15 -0500

    fix typo in function

--- src/reducers/projectReducer.js
+++ src/reducers/projectReducer.js
@@ -1,7 +1,9 @@
 export default function projectReducer(state={all_projects: []}, action) {
   switch (action.type) {
-    case 'ADD_PROJECTS': {
-      debugger
+    case 'ADD_PROJECTS':
+    // debugger
+      return {
+      ...state, payload: true
     }
     default: {
       return state
