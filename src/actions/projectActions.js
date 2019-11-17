ca6b1660310265c566cfcc4ac20a77a8ce257394
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2019-11-16 21:49:00 -0500

    add dispatch method

--- src/actions/projectActions.js
+++ src/actions/projectActions.js
@@ -1,7 +1,11 @@
 export const getProjects = () => {
-  return dispatch => {
-    return (  fetch("http://localhost:3001/projects")
+  return dispatch => {dispatch({type: "ADD_PROJECTS"})
+    return (fetch("http://localhost:3001/projects")
       .then(response => response.json())
       .then(data=>dispatch({type: "ADD_PROJECTS", payload: data })))
   }
+}
+
+export const postComments = () => {
+
 }
