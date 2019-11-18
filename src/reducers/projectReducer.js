2a20ed550ffcca1d6156f0aba7a3a16a292925db
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2019-11-17 23:45:04 -0500

    add cases to reducer: get, add, delete

--- src/reducers/projectReducer.js
+++ src/reducers/projectReducer.js
@@ -1,12 +1,27 @@
-export default function projectReducer(state={all_projects: []}, action) {
+import * as actions from '../actions/projectActions';
+
+const initialValues = {
+  allProjects: [], error: ''
+}
+
+const projectReducer = (state = initialValues, action) => {
   switch (action.type) {
-    case 'ADD_PROJECTS':
-    // debugger
-      return {
-      ...state, payload: true
-    }
+    case actions.GET_PROJECTS_SUCCESS:
+      return { ...state, allProjects: action.payload }
+    case actions.GET_PROJECTS_FAILURE:
+      return { ...state, error: action.payload}
+    case actions.ADD_PROJECT_SUCCESS:
+      return { ...state, allProjects: state.allProjects.concat(action.payload) }
+    case actions.ADD_PROJECT_FAILURE:
+      return { ...state, error: action.payload}
+    case actions.DELETE_PROJECT_SUCCESS:
+      return {...state, allProjects: state.allProjects.filter(p => p.id !== action.payload)}
+    case actions.DELETE_PROJECT_FAILURE:
+      return {...state, error: action.payload}
     default: {
       return state
     }
   }
 }
+
+export default projectReducer;
